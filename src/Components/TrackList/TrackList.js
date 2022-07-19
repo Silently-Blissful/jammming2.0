781e129305b1173329edf20d5ecd8fbefd8f0314
Silently-Blissful <93438995+Silently-Blissful@users.noreply.github.com>
2022-07-19 16:43:56 -0600

    Continue Project

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,23 @@
 import React from "react";
 import './TrackList.css';
+import Track from '../Track/Track';
 
 class TrackList extends React.Component {
   render() {
+    let listOfTracks = this.props.tracks.map((track) => {
+      return (
+        <Track
+          track={ track }
+          key={ track.id }
+          onAdd={ this.props.onAdd }
+          onRemove={ this.props.onRemove }
+          isRemoval={ this.props.isRemoval }
+        />
+      );
+    });
     return (
       <div className="TrackList">
-        <p>map of Track components</p>
+        { listOfTracks }
       </div>
     )
   }
---
+++ src/util/Spotify.js
@@ -0,0 +1,93 @@
+const clientId = '4b77e027f4ed472d9d47aaa694c6eafc';
+const redirect_uri = 'http://localhost:3000/';
+
+let token;
+
+const Spotify ={
+  getAccessToken() {
+    if(token) {
+      return token;
+    }
+    const newToken = window.location.href.match(/access_token=([^&]*)/);
+    const newTokenExpires = window.location.href.match(/expires_in=([^&]*)/);
+    if (newToken && newTokenExpires) {
+      token = newToken[1];
+      const expiresIn = Number(newTokenExpires[1]);
+      window.setTimeout(() => token = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return token;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&show_dialog=trust&redirect_url=${redirect_uri}`;
+      window.location = accessUrl;
+    };
+  },
+
+  search(term) {
+    const token = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${token}` };
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, { headers: headers }).then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+      throw new Error('Request failed!');
+    }, networkError => {
+      console.log(networkError.message);
+    }).then(jsonResponse => {
+      if (!jsonResponse.tracks) {
+        return [];
+      }
+      return jsonResponse.tracks.items.map((track) => ({ id: track.id, name: track.name, artist: track.artist[0].name, album: track.album.name, uri: track.uri }));
+    });
+  },
+
+  savePlaylist(playlistName, trackURIs) {
+    if (playlistName && trackURIs.length) {
+      const accessToken = Spotify.getAccessToken();
+      const headers = {
+        Authorization: `Bearer ${accessToken}`
+      };
+      let userID;
+      let playlistID;
+      return fetch('https://api.spotify.com/v1/me', { headers: headers }).then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Request failed!');
+      }, networkError => {
+        console.log(networkError.message);
+      }).then(jsonResponse => {
+        userID = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userID}/playlist`, {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify({ name: playlistName })
+        }).then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Request failed!');
+        }, networkError => {
+          console.log(networkError.message);
+        }).then(jsonResponse => {
+          playlistID = jsonResponse.id;
+          return fetch(`https://api.spotify.com/v1/users/${userID}/playlist/${playlistID}/tracks`, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify({ uris: trackURIs })
+          }).then(response => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error('Request failed!');
+          }, networkError => {
+            console.log(networkError.message);
+          }).then(jsonResponse => jsonResponse);
+        });
+      });
+    } else {
+      return;
+    };
+  }
+};
+
+export default Spotify;
